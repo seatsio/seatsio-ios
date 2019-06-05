1014e3cc5c6f0ce4a18a60afea83b894518340d2
Ben Verbeken <ben@seats.io>
2019-06-05 12:56:27 +0200

    loading seatingchart in wkwebview

--- seatsio-ios/ViewController.swift
+++ seatsio-ios/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController,WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let html = generateHtml()
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+    func generateHtml() -> String {
+        return """
+        <html>
+        <body>
+        <div id="chart"></div>
+        <script src="https://cdn.seatsio.net/chart.js"></script>
+        <script>
+        new seatsio.SeatingChart({
+        divId: 'chart',
+        publicKey: '53b1ee0f-6611-4826-95a9-197be43a55e7',
+        event: 'e1'
+        }).render();
+        </script>
+        </body>
+        </html>
+        """
     }
 
 
