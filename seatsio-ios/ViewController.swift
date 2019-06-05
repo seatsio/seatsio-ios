9d0d5404d9cf42dbfd402cbe12e6724deaf034fd
Ben Verbeken <ben@seats.io>
2019-06-05 12:44:47 +0200

    Initial Commit

---
+++ seatsio-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seatsio-ios
+//
+//  Created by Ben Verbeken on 05/06/2019.
+//  Copyright © 2019 Ben Verbeken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
