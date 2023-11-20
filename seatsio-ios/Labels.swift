7abc2a8985f057a7720937b3a6b13713a069070d
Sasha Myshkina <sasha.myshkina@dice.fm>
2023-11-20 09:06:46 +0000

    🔧 Add public init for Labels and SeatsioObject

--- seatsio-ios/Labels.swift
+++ seatsio-ios/Labels.swift
@@ -6,4 +6,11 @@ public struct Labels: Decodable {
     public let parent: String?
     public let section: String?
     public let displayedLabel: String
+
+    public init(own: String, parent: String? = nil, section: String? = nil, displayedLabel: String = "") {
+        self.own = own
+        self.parent = parent
+        self.section = section
+        self.displayedLabel = displayedLabel
+    }
 }
