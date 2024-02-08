81a79567c28e2539a87d3f970bff5c88be774203
Sasha Myshkina <sasha.myshkina@dice.fm>
2024-02-08 14:32:18 +0000

    🔧 Add init(from decoder: Decoder) for SeatsioObject

--- seatsio-ios/SeatsioObject.swift
+++ seatsio-ios/SeatsioObject.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 public struct SeatsioObject: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case objectType, label, labels, id, category, center, pricing, status, forSale, selectable, inSelectableChannel, selected, selectedTicketType, extraData, accessible, companionSeat, restrictedView, disabledBySocialDistancingRules, capacity, numBooked, numFree, numSelected, selectionPerTicketType, sectionCategory, numberOfSelectableObjects, numberOfSelectedObjects, selectableCategories, isInteractive
+    }
+
     public let objectType: String
     public let label: String
     public let labels: Labels
@@ -32,4 +37,40 @@ public struct SeatsioObject: Decodable {
     public let numberOfSelectedObjects: Int?
     public let selectableCategories: [Category]?
     public let isInteractive: Bool?
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        objectType = try container.decode(String.self, forKey: CodingKeys.objectType)
+        label = try container.decode(String.self, forKey: CodingKeys.label)
+        labels = try container.decode(Labels.self, forKey: CodingKeys.labels)
+        id = try container.decode(String.self, forKey: CodingKeys.id)
+
+        category = try? container.decode(Category.self, forKey: CodingKeys.category)
+        center = try? container.decode(Point.self, forKey: CodingKeys.center)
+        pricing = try? container.decode(Pricing.self, forKey: CodingKeys.pricing)
+        status = try? container.decode(String.self, forKey: CodingKeys.status)
+        forSale = try? container.decode(Bool.self, forKey: CodingKeys.forSale)
+        selectable = try? container.decode(Bool.self, forKey: CodingKeys.selectable)
+        inSelectableChannel = try? container.decode(Bool.self, forKey: CodingKeys.inSelectableChannel)
+        selected = try? container.decode(Bool.self, forKey: CodingKeys.selected)
+        selectedTicketType = try? container.decode(String.self, forKey: CodingKeys.selectedTicketType)
+        extraData = try? container.decode(Data.self, forKey: CodingKeys.extraData)
+        accessible = try? container.decode(Bool.self, forKey: CodingKeys.accessible)
+        companionSeat = try? container.decode(Bool.self, forKey: CodingKeys.companionSeat)
+        restrictedView = try? container.decode(Bool.self, forKey: CodingKeys.restrictedView)
+        disabledBySocialDistancingRules = try? container.decode(Bool.self, forKey: CodingKeys.disabledBySocialDistancingRules)
+
+        capacity = try? container.decode(Int.self, forKey: CodingKeys.capacity)
+        numBooked = try? container.decode(Int.self, forKey: CodingKeys.numBooked)
+        numFree = try? container.decode(Int.self, forKey: CodingKeys.numFree)
+        numSelected = try? container.decode(Int.self, forKey: CodingKeys.numSelected)
+        selectionPerTicketType = try? container.decode([String:Int].self, forKey: CodingKeys.selectionPerTicketType)
+
+        sectionCategory = try? container.decode(Category.self, forKey: CodingKeys.sectionCategory)
+        numberOfSelectableObjects = try? container.decode(Int.self, forKey: CodingKeys.numberOfSelectableObjects)
+        numberOfSelectedObjects = try? container.decode(Int.self, forKey: CodingKeys.numberOfSelectedObjects)
+        selectableCategories = try? container.decode([Category].self, forKey: CodingKeys.selectableCategories)
+        isInteractive = try? container.decode(Bool.self, forKey: CodingKeys.isInteractive)
+    }
 }
