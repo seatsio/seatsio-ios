a897e163bc396ea2faf7b2f0e5964fca3d5c8156
Ben Verbeken <ben@seats.io>
2024-04-23 13:10:49 +0200

    added support for descriptions of ticket types (#24)

--- seatsio-ios/TicketTypePricing.swift
+++ seatsio-ios/TicketTypePricing.swift
@@ -5,10 +5,12 @@ public struct TicketTypePricing: Codable {
     let ticketType: String
     let price: Float
     let label: String?
+    let description: String?
 
-    public init(ticketType: String, price: Float, label: String? = nil) {
+    public init(ticketType: String, price: Float, label: String? = nil, description: String? = nil) {
         self.ticketType = ticketType
         self.price = price
         self.label = label
+        self.description = description
     }
 }
