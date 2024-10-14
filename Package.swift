e7937dd72243523f72187508f5ceac9812ef7819
Eric Hovhannisyan <Erichovhannisyan13@gmail.com>
2024-10-14 18:31:40 +0400

    Create package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "seatsio-ios",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "seatsio-ios",
+            targets: ["seatsio-ios"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "seatsio-ios"),
+    ]
+)
