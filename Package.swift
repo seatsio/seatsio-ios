e4135c4d021c37bb6a2ba88a2f225d6c4dccc0cf
Eric Hovhannisyan <Erichovhannisyan13@gmail.com>
2024-10-14 19:14:33 +0400

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "seatsio-ios",
+    name: "Seatsio",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
