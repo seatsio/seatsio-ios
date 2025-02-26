58f3d3af4326be716ab53a0d821df497ec2d10ab
Pablo Neirotti <dotmacintosh@gmail.com>
2025-02-26 10:19:47 +0100

    Added objectPopover.confirmTicketTypeSelection

--- Seatsio/Sources/ObjectPopover.swift
+++ Seatsio/Sources/ObjectPopover.swift
@@ -8,8 +8,9 @@ public struct ObjectPopover: Encodable {
     public let showUnavailableNotice: Bool?
     public let stylizedLabel: Bool?
     public let confirmSelection: String?
+    public let confirmTicketTypeSelection: Bool?
 
-    public init(showAvailability: Bool? = false, showCategory: Bool? = true, showLabel: Bool? = true, showPricing: Bool? = true, showUnavailableNotice: Bool? = true, stylizedLabel: Bool? = true, confirmSelection: String? = "auto") {
+    public init(showAvailability: Bool? = false, showCategory: Bool? = true, showLabel: Bool? = true, showPricing: Bool? = true, showUnavailableNotice: Bool? = true, stylizedLabel: Bool? = true, confirmSelection: String? = "auto", confirmTicketTypeSelection: Bool? = false) {
         self.showAvailability = showAvailability
         self.showCategory = showCategory
         self.showLabel = showLabel
@@ -17,5 +18,6 @@ public struct ObjectPopover: Encodable {
         self.showUnavailableNotice = showUnavailableNotice
         self.stylizedLabel = stylizedLabel
         self.confirmSelection = confirmSelection
+        self.confirmTicketTypeSelection = confirmTicketTypeSelection
     }
 }
