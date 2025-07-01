d28ece2ff8f115e645bc9002bfb0255c3594e48c
Scott Divelbiss <scottdivelbiss@gmail.com>
2025-07-01 12:55:03 +0200

    add primary to TicketTypePricing

--- Seatsio/Sources/TicketTypePricing.swift
+++ Seatsio/Sources/TicketTypePricing.swift
@@ -6,11 +6,13 @@ public struct TicketTypePricing: Codable {
     public let price: Float
     public let label: String?
     public let description: String?
+    public let primary: Bool?
 
-    public init(ticketType: String, price: Float, label: String? = nil, description: String? = nil) {
+    public init(ticketType: String, price: Float, label: String? = nil, description: String? = nil, primary: Bool? = nil) {
         self.ticketType = ticketType
         self.price = price
         self.label = label
         self.description = description
+        self.primary = primary
     }
 }
