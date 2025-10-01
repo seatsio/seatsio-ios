50a912b528302f937082bf652ac63769b6622ff0
Steve Chaloner <steve@seats.io>
2025-10-01 15:47:37 +0200

    Use separate class to Pricing for the object pricing property

---
+++ Seatsio/Sources/SeatsioObjectPricing.swift
@@ -0,0 +1,38 @@
+//
+//  SeatsioObjectPricing.swift
+//  seatsio-ios
+//
+//  Created by Steve Chaloner on 01/10/2025.
+//
+
+
+import Foundation
+
+public struct SeatsioObjectPricing: Codable {
+    
+    public let ticketType: String?
+    public let price: Float?
+    public let formattedPrice: String?
+    public let originalPrice: Float?
+    public let formattedOriginalPrice: String?
+    public let label: String?
+    
+    public init(ticketType: String? = nil, price: Float? = nil, formattedPrice: String? = nil, originalPrice: Float? = nil, formattedOriginalPrice: String? = nil,
+ label: String? = nil) {
+        self.ticketType = ticketType
+        self.price = price
+        self.formattedPrice = formattedPrice
+        self.originalPrice = originalPrice
+        self.formattedOriginalPrice = formattedOriginalPrice
+        self.label = label
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case ticketType
+        case price
+        case formattedPrice
+        case originalPrice
+        case formattedOriginalPrice
+        case label
+    }
+}
