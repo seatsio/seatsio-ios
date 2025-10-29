618efa14087c6c70aeb783de94baeaff3ac32ac4
Steve Chaloner <steve@seats.io>
2025-10-29 11:18:28 +0000

    Added missing properties to SeatsioObjectPricing

--- Seatsio/Sources/SeatsioObjectPricing.swift
+++ Seatsio/Sources/SeatsioObjectPricing.swift
@@ -16,15 +16,19 @@ public struct SeatsioObjectPricing: Codable {
     public let originalPrice: Float?
     public let formattedOriginalPrice: String?
     public let label: String?
+    public let fee: Float?
+    public let description: String?
     
     public init(ticketType: String? = nil, price: Float? = nil, formattedPrice: String? = nil, originalPrice: Float? = nil, formattedOriginalPrice: String? = nil,
- label: String? = nil) {
+                label: String? = nil, fee: Float? = nil, description: String? = nil) {
         self.ticketType = ticketType
         self.price = price
         self.formattedPrice = formattedPrice
         self.originalPrice = originalPrice
         self.formattedOriginalPrice = formattedOriginalPrice
         self.label = label
+        self.fee = fee
+        self.description = description
     }
     
     private enum CodingKeys: String, CodingKey {
@@ -34,5 +38,7 @@ public struct SeatsioObjectPricing: Codable {
         case originalPrice
         case formattedOriginalPrice
         case label
+        case fee
+        case description
     }
 }
