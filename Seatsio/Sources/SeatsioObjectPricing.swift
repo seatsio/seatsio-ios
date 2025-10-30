6891ad49ab4ad56adf41629c41fcecfa6b947ba1
Ben Verbeken <ben@seats.io>
2025-10-30 13:10:42 +0100

    Json decode fix (#48)
    * Add StringOrFloat typing
    
    * Added decoding init for SeatsiObjectPricing
    
    * Fix JSON decoding for optional properties in SeatsioObjectPricing
    
    ---------
    
    Co-authored-by: Morten Devold <morten@seats.io>
    Co-authored-by: Steve Chaloner <steve@seats.io>

--- Seatsio/Sources/SeatsioObjectPricing.swift
+++ Seatsio/Sources/SeatsioObjectPricing.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public struct SeatsioObjectPricing: Codable {
-    
+
     public let ticketType: String?
     public let price: Float?
     public let formattedPrice: String?
@@ -18,7 +18,7 @@ public struct SeatsioObjectPricing: Codable {
     public let label: String?
     public let fee: Float?
     public let description: String?
-    
+
     public init(ticketType: String? = nil, price: Float? = nil, formattedPrice: String? = nil, originalPrice: Float? = nil, formattedOriginalPrice: String? = nil,
                 label: String? = nil, fee: Float? = nil, description: String? = nil) {
         self.ticketType = ticketType
@@ -30,7 +30,27 @@ public struct SeatsioObjectPricing: Codable {
         self.fee = fee
         self.description = description
     }
-    
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        ticketType = try container.decodeIfPresent(String.self, forKey: .ticketType)
+        price = try container.decodeIfPresent(Float.self, forKey: .price)
+        if let value = try? container.decodeIfPresent(Float.self, forKey: .formattedPrice) {
+            formattedPrice = String(value)
+        } else {
+            formattedPrice = try container.decodeIfPresent(String.self, forKey: .formattedPrice)
+        }
+        originalPrice = try container.decodeIfPresent(Float.self, forKey: .originalPrice)
+        if let value = try? container.decodeIfPresent(Float.self, forKey: .formattedOriginalPrice) {
+            formattedOriginalPrice = String(value)
+        } else {
+            formattedOriginalPrice = try container.decodeIfPresent(String.self, forKey: .formattedOriginalPrice)
+        }
+        label = try container.decodeIfPresent(String.self, forKey: .label)
+        fee = try container.decodeIfPresent(Float.self, forKey: .fee)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+    }
+
     private enum CodingKeys: String, CodingKey {
         case ticketType
         case price
@@ -42,3 +62,4 @@ public struct SeatsioObjectPricing: Codable {
         case description
     }
 }
+
