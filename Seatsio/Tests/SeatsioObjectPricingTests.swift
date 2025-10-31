6da06aa2c94b3f9d1f84f9742ce64f59790b1d15
Ben Verbeken <ben@seats.io>
2025-10-31 16:23:18 +0100

    more unit tests

--- Seatsio/Tests/SeatsioObjectPricingTests.swift
+++ Seatsio/Tests/SeatsioObjectPricingTests.swift
@@ -4,24 +4,21 @@ import XCTest
 final class SeatsioObjectPricingTests: XCTestCase {
 
     func testInitFromJSON_withStringFormattedPrice() throws {
-        // Given
         let json = """
         {
-            "ticketType": "VIP",
-            "price": 120.5,
-            "formattedPrice": "$120.50",
-            "originalPrice": 150.0,
-            "formattedOriginalPrice": "$150.00",
-            "label": "Early Bird",
-            "fee": 5.0,
-            "description": "Front row"
-        }
+                    "ticketType": "VIP",
+                    "price": 120.5,
+                    "formattedPrice": "$120.50",
+                    "originalPrice": 150.0,
+                    "formattedOriginalPrice": "$150.00",
+                    "label": "Early Bird",
+                    "fee": 5.0,
+                    "description": "Front row"
+                }
         """.data(using: .utf8)!
 
-        // When
         let decoded = try JSONDecoder().decode(SeatsioObjectPricing.self, from: json)
 
-        // Then
         XCTAssertEqual(decoded.ticketType, "VIP")
         XCTAssertEqual(decoded.price, 120.5)
         XCTAssertEqual(decoded.formattedPrice, "$120.50")
@@ -33,19 +30,64 @@ final class SeatsioObjectPricingTests: XCTestCase {
     }
 
     func testInitFromJSON_withNumericFormattedPrice() throws {
-        // Given
         let json = """
         {
-            "formattedPrice": 120.5,
-            "formattedOriginalPrice": 150.0
-        }
+                    "formattedPrice": 120.5,
+                    "formattedOriginalPrice": 150.0
+                }
         """.data(using: .utf8)!
 
-        // When
         let decoded = try JSONDecoder().decode(SeatsioObjectPricing.self, from: json)
 
-        // Then
         XCTAssertEqual(decoded.formattedPrice, "120.5")
         XCTAssertEqual(decoded.formattedOriginalPrice, "150.0")
+    }
+
+    func testInitFromJSON_withoutFormattedPriceFields() throws {
+        let json = """
+        {
+                    "ticketType": "Standard",
+                    "price": 90.0,
+                    "originalPrice": 100.0
+                }
+        """.data(using: .utf8)!
+
+        let decoded = try JSONDecoder().decode(SeatsioObjectPricing.self, from: json)
+
+        XCTAssertEqual(decoded.ticketType, "Standard")
+        XCTAssertEqual(decoded.price, 90.0)
+        XCTAssertEqual(decoded.originalPrice, 100.0)
+        XCTAssertNil(decoded.formattedPrice)
+        XCTAssertNil(decoded.formattedOriginalPrice)
+    }
+
+    func testInitFromJSON_onlyFormattedPricePresent() throws {
+        let json = """
+        {
+                    "formattedPrice": "$45.00"
+                }
+        """.data(using: .utf8)!
+
+        let decoded = try JSONDecoder().decode(SeatsioObjectPricing.self, from: json)
+
+        XCTAssertEqual(decoded.formattedPrice, "$45.00")
+        XCTAssertNil(decoded.formattedOriginalPrice)
+        XCTAssertNil(decoded.price)
+        XCTAssertNil(decoded.originalPrice)
+    }
+
+    func testInitFromJSON_onlyFormattedOriginalPricePresent() throws {
+        let json = """
+        {
+                    "formattedOriginalPrice": "$150.00"
+                }
+        """.data(using: .utf8)!
+
+        let decoded = try JSONDecoder().decode(SeatsioObjectPricing.self, from: json)
+
+        XCTAssertNil(decoded.formattedPrice)
+        XCTAssertEqual(decoded.formattedOriginalPrice, "$150.00")
+        XCTAssertNil(decoded.price)
+        XCTAssertNil(decoded.originalPrice)
     }
 }
